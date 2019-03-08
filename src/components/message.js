3a10057a7e378b305f79a1092516d9523ae2d9af
Usheninte <jrandangana@gmail.com>
2019-03-08 15:48:09 +0100

    added

--- src/components/message.js
+++ src/components/message.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
+const one = {
+  width: '10em',
+  height: 'auto',
+  padding: '1em',
+  margin: '2em',
+  textAlign: 'center',
+  backgroundColor: '#61dafb'
+}
+
+const two = {
+  backgroundColor: '#3cb371'
+}
+
+const three = {
+  color: '#f0f0f0',
+  backgroundColor: '#212121'
+}
+
 const Message = () => {
   return(
-    <div>Hi there</div>
+    <div>
+      <h3 style={one}>Hello</h3>
+      <h3 style={two}>Hi</h3>
+      <h3 style={three}>Yo!</h3>
+    </div>
   );
 }
 
